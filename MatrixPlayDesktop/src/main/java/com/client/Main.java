9d28f04a611ce98b6def3f522cc22ee6dcc73dc7
laurattt <lauratoro422@gmail.com>
2025-11-10 15:24:34 +0100

    project structure

---
+++ MatrixPlayDesktop/src/main/java/com/client/Main.java
@@ -0,0 +1,19 @@
+package com.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+        // Iniciar app JavaFX   
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+       
+    }
+
+}
